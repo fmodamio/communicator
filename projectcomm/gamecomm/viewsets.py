c53f7882632fbad76c86e7da641557a249233eee
Fernando <fernando.modamio@gmail.com>
2021-08-17 20:41:12 +0200

    change allowed methods on player

--- projectcomm/gamecomm/viewsets.py
+++ projectcomm/gamecomm/viewsets.py
@@ -6,6 +6,7 @@ from .serializer import JugadorSerializer, HistoriaSerializer
 class JugadorViewSet(viewsets.ModelViewSet):
 	queryset = Jugador.objects.all()
 	serializer_class = JugadorSerializer
+	http_method_names = ['get']
 
 class HistoriaViewSet(viewsets.ModelViewSet):
 	queryset = Historia.objects.all()
