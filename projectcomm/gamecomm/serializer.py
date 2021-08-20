d7ba872319379d38014d4a3f6d262486865dc69d
Fernando <fernando.modamio@gmail.com>
2021-08-20 12:17:09 +0200

    parametros get en front

--- projectcomm/gamecomm/serializer.py
+++ projectcomm/gamecomm/serializer.py
@@ -4,13 +4,12 @@ from .models import Jugador, Historia
 class JugadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jugador
-        fields = ['nombre']
+        fields = ['id','nombre']
 
     def create(self, validated_data):
         profile_data = validated_data.pop('nombre')
-        user = Jugador.objects.create(nombre=profile_data,respeto = 1, alimento = 1, dinero = 1, energia = 1, dopamina = 1, sueno = 1,
-    supmoral = 1, soberbia = 1, gula = 1, avaricia = 1, ira = 1, lujuria = 1, pereza = 1, envidia = 1, )
-        return user
+        return Jugador.objects.create(nombre=profile_data,respeto = 1, alimento = 1, dinero = 1, energia = 1, dopamina = 1, sueno = 1,
+    supmoral = 1, soberbia = 1, gula = 1, avaricia = 1, ira = 1, lujuria = 1, pereza = 1, envidia = 1)
 
 class HistoriaSerializer(serializers.ModelSerializer):
     class Meta:
