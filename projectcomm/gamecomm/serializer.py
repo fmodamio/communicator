59a8b09bdd553f5d70e6fb0e870494604efdb5a2
Fernando <fernando.modamio@gmail.com>
2021-08-26 14:00:47 +0200

    bleach para escapar la entrada de nombres de usuario

--- projectcomm/gamecomm/serializer.py
+++ projectcomm/gamecomm/serializer.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Jugador, Historia
 
+import bleach
+
 class JugadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jugador
         fields = ['id','nombre']
 
     def create(self, validated_data):
-        nombrepost = validated_data.pop('nombre')
+        nombrepost = bleach.clean(validated_data.pop('nombre'))
+
         #esto hay que cambiarlo en el modelo, usando default
         return Jugador.objects.create(nombre=nombrepost,respeto = 1, alimento = 1, dinero = 1, energia = 1, dopamina = 1, sueno = 1,
     supmoral = 1, soberbia = 1, gula = 1, avaricia = 1, ira = 1, lujuria = 1, pereza = 1, envidia = 1)
